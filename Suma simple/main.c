b8becf8b7ba2673c0d3565d985e9f8bc47be7556
Franz Fernandez Cobarrubia <franzcobarrubia@gmail.com>
2025-03-17 21:13:49 +0100

    Problema 453

---
+++ Suma simple/main.c
@@ -0,0 +1,16 @@
+/*
+ * Author: Franz Fernandez Cobarrubia
+ * Date: 17-03-2025
+ */
+
+#include <stdio.h>
+
+
+int main(void)
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    printf("%d\n", a + b);
+    return 0;
+}
