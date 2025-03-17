8bfae2b403baf0a40cfb5c49774d6d9ba84da98a
Franz Fernandez Cobarrubia <franzcobarrubia@gmail.com>
2025-03-17 21:52:50 +0100

    Problema 9332

---
+++ La gran maratonista/main.c
@@ -0,0 +1,20 @@
+/*
+ * Author: Franz Fernandez Cobarrubia
+ * Date: 17-03-2025
+ */
+
+#include <stdio.h>
+
+void solve(void)
+{
+    long long int n, d, dist;
+    scanf("%lld %lld", &n, &d);
+
+    printf("%lld\n", n * d);
+}
+
+int main(void)
+{
+    solve();
+    return 0;
+}
