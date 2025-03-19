96bbf09cfb78a0bc8a4b92c4de27b2a44210139d
Franz Fernandez Cobarrubia <franzcobarrubia@gmail.com>
2025-03-19 21:34:50 +0100

    Problema 7391

---
+++ Mod de dos números/main.c
@@ -0,0 +1,20 @@
+/*
+ * Author: Franz Fernandez Cobarrubia
+ * Date: 19-03-2025
+ */
+
+#include <stdio.h>
+
+void solve(void)
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    printf("%d\n", a % b);
+}
+
+int main(void)
+{
+    solve();
+    return 0;
+}
