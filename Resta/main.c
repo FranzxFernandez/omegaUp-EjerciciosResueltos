ec98e45209a3f5bfa668ecf103d5e9ce030ece56
Franz Fernandez Cobarrubia <franzcobarrubia@gmail.com>
2025-03-22 14:48:11 +0100

    Problema 2468

---
+++ Resta/main.c
@@ -0,0 +1,15 @@
+/*
+ * Author: Franz Fernandez Cobarrubia
+ * Date: 22-03-2025
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    
+    printf("%d\n", a - b);
+    return 0;
+}
