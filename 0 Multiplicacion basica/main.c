df499608de826d285fcbceacf6df0bcc5a2e21b1
Franz Fernandez Cobarrubia <franzcobarrubia@gmail.com>
2025-03-28 14:56:50 +0100

    Problema 772

---
+++ 0 Multiplicacion basica/main.c
@@ -0,0 +1,20 @@
+/*
+ * Author: Franz Fernandez Cobarrubia
+ * Date: 28-03-2025
+ */
+
+#include <stdio.h>
+
+void solve(void)
+{
+    int a, b;
+    scanf("%d%d", &a, &b);
+
+    printf("%d", a * b);
+}
+
+int main(void)
+{
+    solve();
+    return 0;
+}
