e22323eb3ea8861533d29fe87e95a41f0b1ea29d
Franz Fernandez Cobarrubia <franzcobarrubia@gmail.com>
2025-04-13 22:54:13 +0200

    Problema 5127

---
+++ A Contar Lápices 2/main.c
@@ -0,0 +1,94 @@
+/*
+ * Author: Franz Fernandez Cobarrubia
+ * Date: 12-04-2025
+ */
+
+#include <stdio.h>
+
+// Useful macros
+#define max(a, b) ((a) > (b) ? (a) : (b))
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define abs(x)    ((x) < 0 ? -(x) : (x))
+
+// #define swap(a, b) {typeof(a) temp = a; a = b; b = temp;}
+
+// Fast input (both positive and negative integers and better for large inputs)
+// int readint(void) {
+//     int n = 0, sign = 1;
+//     int c = getchar();
+//     if (c == '-') {
+//         sign = -1;
+//         c = getchar();
+//     }
+//     while (c >= '0' && c <= '9') {
+//         n = n * 10 + (c - '0');
+//         c = getchar();
+//     }
+//     return n * sign;
+// }
+
+int readint(void) {
+    int n = 0, c;
+    while ((c = getchar()) >= '0' && c <= '9') {
+        n = n * 10 + c - '0';
+    }
+    return n;
+}
+
+// Fast string input (only to read a single word with no spaces)
+void readstr(char* str) {
+    char c;
+    while ((c = getchar()) > ' ') {
+        *str++ = c;
+    }
+    *str = 0;
+}
+
+// Comparison function for qsort (ascending)
+int compare_asc(const void* a, const void* b) {
+    int x = *(int*)a;
+    int y = *(int*)b;
+    if (x < y) {
+        return -1;
+    }
+    if (x > y) {
+        return 1;
+    }
+    return 0;
+}
+
+// Comparison function for qsort (descending)
+int compare_desc(const void* a, const void* b) {
+    int x = *(int*)a;
+    int y = *(int*)b;
+    if (x < y) {
+        return 1;
+    }
+    if (x > y) {
+        return -1;
+    }
+    return 0;
+}
+
+/*////////////////////////////////////////////////////////////*/
+
+void solve(void) {
+    int n = readint();
+    int k = readint();
+
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        int value = readint();
+        if (value == k) {
+            count++;
+        }
+    }
+
+    printf("%d\n", count);
+}
+
+int main(void) {
+    solve();
+    return 0;
+}
