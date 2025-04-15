99665c7744bb4fa8b0dc7f06be238505a3ca59ef
Franz Fernandez Cobarrubia <franzcobarrubia@gmail.com>
2025-04-15 22:33:19 +0200

    Problema 7151

---
+++ Una sumatoria muy extraña/main.c
@@ -0,0 +1,92 @@
+/*
+ * Author: Franz Fernandez Cobarrubia
+ * Date: 15-04-2025
+ */
+
+#include <stdio.h>
+
+// Useful macros
+#define max(a, b) ((a) > (b) ? (a) : (b))
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define abs(x)    ((x) < 0 ? -(x) : (x))
+
+// #define swap(a, b) {typeof(a) temp = a; a = b; b = temp;}
+
+// Fast input (both positive and negative integers and better for large inputs)
+int readint(void) {
+    int n = 0, sign = 1;
+    int c = getchar();
+    // if (c == '-') {
+    //     sign = -1;
+    //     c = getchar();
+    // }
+    while (c >= '0' && c <= '9') {
+        n = n * 10 + (c - '0');
+        c = getchar();
+    }
+    return n;
+}
+
+// Fast string input (only to read a single word with no spaces)
+void readstr(char* str) {
+    char c;
+    while ((c = getchar()) > ' ') {
+        *str++ = c;
+    }
+    *str = 0;
+}
+
+// Comparison function for qsort (ascending)
+int compare_asc(const void* a, const void* b) {
+    int x = *(int*)a;
+    int y = *(int*)b;
+    if (x < y) {
+        return -1;
+    }
+    if (x > y) {
+        return 1;
+    }
+    return 0;
+}
+
+// Comparison function for qsort (descending)
+int compare_desc(const void* a, const void* b) {
+    int x = *(int*)a;
+    int y = *(int*)b;
+    if (x < y) {
+        return 1;
+    }
+    if (x > y) {
+        return -1;
+    }
+    return 0;
+}
+
+/*////////////////////////////////////////////////////////////*/
+
+void solve(void) {
+    int a = readint();
+    int b = readint();
+
+    int m = 0;
+    int n = 0;
+    for (int i = 1; i <= a; i++) {
+        m += i;
+    }
+
+    for (int i = 1; i <= b; i++) {
+        n += i;
+    }
+
+    int total = 0;
+    for (int i = m; i <= n; i++) {
+        total += i;
+    }
+
+    printf("%d\n", total);
+}
+
+int main(void) {
+    solve();
+    return 0;
+}
